a02579ab4354b810ff3496649620b7d93419eccd
Juanjo Marti <jv@vlot.ch>
2020-05-12 20:19:04 +0200

    Update todo list

--- sections/todos/src/components/form.js
+++ sections/todos/src/components/form.js
@@ -1,31 +1,35 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import styles from './form.module.css';
 
-const Form = ({reloadTodos}) => {
-
+const Form = ({ reloadTodos }) => {
     const [text, setText] = useState('');
 
     const handleSubmit = async event => {
         event.preventDefault();
-        if(text === '') return;
 
-        await axios.post('/api/create-todo', {text});
+        if (text === '') return;
+
+        await axios.post('/api/create-todo', { text });
 
         setText('');
         reloadTodos();
     };
 
     return (
-        <form className={styles.form} onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} className={styles.form}>
             <label className={styles.label}>
                 Add a Todo
-                <input type="text" className={styles.input} value={text} onChange={event => setText(event.target.value)}/>
+                <input
+                    type="text"
+                    value={text}
+                    onChange={event => setText(event.target.value)}
+                    className={styles.input}
+                />
             </label>
-            <button className={styles.button}>Save Todos</button>
+            <button className={styles.button}>Save Todo</button>
         </form>
-    )
+    );
 };
-
 
 export default Form;
--- sections/todos/src/pages/index.js
+++ sections/todos/src/pages/index.js
@@ -1,48 +1,65 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import styles from './index.module.css';
-import Todo from '../components/todo';
 import Form from '../components/form';
+import Todo from '../components/todo';
+import styles from './index.module.css';
 
 export default () => {
-
     const [status, setStatus] = useState('loading');
     const [todos, setTodos] = useState(null);
 
-    useEffect(()=>{
+    /*
+     * This pattern comes from David Khourshid. It allows us to avoid what he
+     * calls “juggling hooks” between this and the Form component and allows
+     * requests to be cancelable.
+     *
+     * See https://twitter.com/jlengstorf/status/1185705298868129793 for the
+     * original question on how to manage forms triggering refetching and
+     * https://twitter.com/DavidKPiano/status/1185711350804291584 for David’s
+     * explanation and code sample.
+     *
+     * Shout out to Ryan Florence for additional feedback on handling initial
+     * states: https://twitter.com/ryanflorence/status/1185708671503720448
+     */
+    useEffect(() => {
         let canceled = false;
-        if(status !== 'loading') return;
 
-        axios('/api/get-all-todos').then(result =>{
-            if(canceled === true) return;
+        // only load todos if we’re in the loading state
+        if (status !== 'loading') return;
 
-            if(result.status !== 200) {
-                console.error('error loading todos!');
-                console.error(result);
+        axios('/api/get-all-todos').then(result => {
+            if (canceled === true) return;
+
+            if (result.status !== 200) {
+                console.log('Error loading todos!');
+                console.log(result);
                 return;
             }
+
             setTodos(result.data.todos);
-            setStatus('loaded')
+            setStatus('loaded');
         });
 
-        return ()=> {canceled = true;}
-
+        return () => {
+            // if the component unmounts, we want to cancel any active loads
+            canceled = true;
+        };
     }, [status]);
-
-    const reloadTodos = () => setStatus('loading')
 
     return (
         <main>
-            <h1 className={styles.heading}>JAMstack TODOS</h1>
-            <Form reloadTodos={reloadTodos} />
-            {todos ? (<ul className={styles.todos}>
-                {todos.map(todo => (
-                    <li key={todo._id} className={styles.todo}>
-                        <Todo todo={todo} />
-                    </li>
-                ))}
-            </ul>) : (
-                <p className={styles.loading}>Loading todos...</p>
+            <h1 className={styles.heading}>JAMstack Todos</h1>
+            <Form reloadTodos={() => setStatus('loading')} />
+            {todos ? (
+                <ul className={styles.todos}>
+                    {todos.map(todo => (
+                        <li key={todo._id} className={styles.todo}>
+                            <Todo todo={todo} reloadTodos={() => setStatus('loading')} />
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                <p className={styles.loading}>loading todos...</p>
             )}
         </main>
     );
